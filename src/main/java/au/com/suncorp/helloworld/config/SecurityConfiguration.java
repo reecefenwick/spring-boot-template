a097df9620ae2a9a9940521d35311b06bb330a1f
FENWICK, Reece <Reece.FENWICK@suncorp.com.au>
2015-12-21 15:03:14 +1000

    Don't leak server header

---
+++ src/main/java/au/com/suncorp/helloworld/config/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package au.com.suncorp.helloworld.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends
+        WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        System.out.println("helloreece");
+        http
+            .headers()
+                .addHeaderWriter(new StaticHeadersWriter("server", "0"));
+    }
+}
